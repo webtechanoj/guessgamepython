aad4f495306eee37c291c5da50d1d46f3b026d6e
Anoj Khadka <webtech@anojkhadka.com>
2019-07-09 22:24:33 +1000

    Update game.py
    Added comment - Testing Github Desktop

--- game.py
+++ game.py
@@ -1,25 +1,23 @@
 # Guess game Modified readme
+# Testing Github
 import random
 
-guessNumber = (random.randint(1,50))
+guessNumber = (random.randint(1, 50))
 
 playerName = input("Hey Player, what's your name: ")
-print ("Hello, "+ playerName + ", I am thinking of a number between 1 and 20")
+print("Hello, " + playerName + ", I am thinking of a number between 1 and 20")
 print("Take a guess.")
 i = 0
 print(guessNumber)
-while i<=5:
+while i <= 5:
     i += 1
     guess = int(input())
     if guess > guessNumber:
-        print("Your choice is high, you have "+ str(5-i) + " attempts")
+        print("Your choice is high, you have " + str(5-i) + " attempts")
 
     elif guess < guessNumber:
-        print("Your choice is low, you have "+ str(5-i) + " attempts")
+        print("Your choice is low, you have " + str(5-i) + " attempts")
 
     else:
-        print ("Bravooooooo!!! You guessed it in  " + str(i) + " attempt" )
+        print("Bravooooooo!!! You guessed it in  " + str(i) + " attempt")
         break
-
-
-
